ead978fe35dcaed8c06180d3ab77335989889533
yulyasystem <yulyasystem@gmail.com>
2019-08-26 16:17:54 +0300

    add reviealing animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 import "../sass/main";
 
-console.log('Hello main.js!');
+let links = document.querySelectorAll(".reveal-block__link");
+let contentOfBlock = document.querySelector(".reveal-block__text");
+let main = document.querySelector(".reveal-block");
+
+function toggleClass() {
+  this.classList.toggle(".active");
+}
+
+main.addEventListener("click", ({ target }) => {
+  target.nextElementSibling.classList.toggle("active");
+  console.log(target.nextElementSibling, "target");
+});
